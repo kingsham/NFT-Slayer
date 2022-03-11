a4840b30ed78387a775d212abf5a3708fbf78e44
SHAM <shamarknibbs@gmail.com>
2022-03-11 14:45:35 -0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,16 +3,16 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(
   
       ["Travis","Henry","Shinobu","DrPeace","Badgirl"],
-      ["https://imgur.com/a/aIDUra5.png", // TRAVIS 
-      "https://imgur.com/a/PFkFshv.png", // HENRY
-      "https://imgur.com/a/R2Antmo.png", // SHINOBU
-      "https://imgur.com/a/Agopx7L.png", // DR PEACE
-      "https://imgur.com/a/FZgMn2I.png"], // BADGIRL
+      ["https://imgur.com/v9uyM4M.png", // TRAVIS 
+      "https://imgur.com/yZ9mfdg .png", // HENRY
+      "https://i.imgur.com/8S1Ub2C.png", // SHINOBU
+      "https://imgur.com/F6beVAr.png", // DR PEACE
+      "https://imgur.com/2185BRb.png"], // BADGIRL
       
-      [100,100,100,100,100,100],
-      [100,50,25,50,25,50],
+      [100,100,100,100,100],
+      [100,50,25,50,25],
       "Lord Fu",
-      "https://imgur.com/a/kBZvrTY",
+      "https://i.imgur.com/cOnd0KT.png",
        10000, //Boss hp
        50 // Boss attack dmg
   
